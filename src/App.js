ac951976c199373bc64c2a9c160a3959eb5c593f
pavigitup <pavithragokali@gmail.com>
2024-01-01 17:12:17 +0000

    v1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="container">
+        <Welcome />
+        {isLoggedIn && <button type="button">Logout</button>}
+        {!isLoggedIn && <button type="button">Login</button>}
+      </div>
+    )
+  }
+}
+
+export default App
